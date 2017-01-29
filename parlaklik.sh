e6da1b9581fca9cb981d2bb486b77c408710bd9d
ilhanakilli <ilhanakilli@gmail.com>
2017-01-29 11:30:38 +0200

    Update and rename backlight.sh to parlaklik.sh

---
+++ parlaklik.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir ~/bin
+touch ~/bin/parlaklık
+chmod +x ~/bin/parlaklık
+echo '
+#!/bin/bash
+level=$1
+#echo “Parlaklık değeri $level”
+if [ $# != 1 ]; then
+echo Kullanım: parlaklık "değer" \(0 ile 255 arasında\) 
+exit 1
+fi
+if [[ $level -ge 0 && $level -le 255 ]]; then
+#echo “Parlaklık değeri  $level”
+echo $level > /sys/class/backlight/rpi_backlight/brightness
+echo Parlaklık değeri $level yapıldı.
+exit 0
+else
+echo Girilen parlaklık değeri $level sınırlar dışında \(0 ile 255 arasında olma$
+exit 1
+fi
+export PATH=$PATH:~/bin' > ~/bin/parlaklık
